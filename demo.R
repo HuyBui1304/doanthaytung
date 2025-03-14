d3cfa797c9c8a96869db614d55730142d8dd77dc
Thanh Tâm <ttam99852@gmail.com>
2025-03-14 17:20:32 +0700

    chuyển đổi dữ liệu

--- demo.R
+++ demo.R
@@ -1,4 +1,17 @@
-df <- read.csv("/Users/huy/Documents/doanthaytung/healthcare-dataset-stroke-data.csv")
-
+# df <- read.csv("/Users/huy/Documents/doanthaytung/healthcare-dataset-stroke-data.csv")
+df <- read.csv("D:/BT/clonegit/doanthaytung/healthcare-dataset-stroke-data.csv")
 head(df)
+str(df)
+
+# chuyển sang dạng factor 
+df$gender <- as.factor(df$gender)
+df$ever_married <- as.factor(df$ever_married)
+df$work_type <- as.factor(df$work_type)
+df$Residence_type <- as.factor(df$Residence_type)
+df$smoking_status <- as.factor(df$smoking_status)
+# xem dữ liệu
+str(df)
+# chuyển sang dạng numeric
+df$bmi <- as.numeric(df$bmi)
+# xem dữ liệu 
 str(df)
