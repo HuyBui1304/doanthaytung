24bb6e5ad72296fca2e3ab113765e65dc4b4a604
Minh Huy <buiminhhuy13042004@gmail.com>
2025-04-16 06:47:00 +0700

    Update render.R

--- render.R
+++ render.R
@@ -1,4 +1,4 @@
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
-rmarkdown::render("report.Rmd", output_format = "word_document")
-rmarkdown::render("report.Rmd", output_format = "pdf_document")
+rmarkdown::render("Phân loại hoạt động con người từ dữ liệu cảm biến điện thoại bằng học máy.Rmd", output_format = "word_document")
+rmarkdown::render("Phân loại hoạt động con người từ dữ liệu cảm biến điện thoại bằng học máy.Rmd", output_format = "pdf_document")
